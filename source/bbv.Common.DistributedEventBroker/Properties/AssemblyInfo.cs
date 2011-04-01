6296c8ba439686be569203936047def1bd383b70
Urs Enzler <urs.enzler@gmail.com>
2011-04-01 17:15:47 +0200

    updated MSpec and Moq in all projects

--- source/bbv.Common.DistributedEventBroker/Properties/AssemblyInfo.cs
+++ source/bbv.Common.DistributedEventBroker/Properties/AssemblyInfo.cs
@@ -17,12 +17,9 @@
 //-------------------------------------------------------------------------------
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("bbv.Common.DistributedEventBroker")]
 [assembly: AssemblyDescription("")]
-
-[assembly: InternalsVisibleTo("bbv.Common.DistributedEventBroker.Test, PublicKey=00240000048000009400000006020000002400005253413100040000010001004d0c2d6ca21b67494250ee32f3fdb05d8e5bc6cb12750f8971129451d815f25a4e561fce0d89d234eade53426a6c8e087427ca307ab8ea293e9dcc172960e64caa3b5c1b79d96f6fe5cb41b28b7c56dc47a113e38190d219897464dc54080d9743110a2be455d349406087e768db19bacbf7ab33df846a96779012bbc4d7bdad")]
