c2b27912d6d564a86de91ca077bb413cfb570a2a
Daniel Marbach <daniel.marbach@openplace.net>
2011-05-28 01:12:26 +0200

    Move executes and withs to syntax file

--- source/bbv.Common.Bootstrapper/Syntax/ISyntax.cs
+++ source/bbv.Common.Bootstrapper/Syntax/ISyntax.cs
@@ -18,6 +18,7 @@
 
 namespace bbv.Common.Bootstrapper.Syntax
 {
+    using System;
     using System.Collections.Generic;
 
     /// <summary>
@@ -27,5 +28,98 @@ namespace bbv.Common.Bootstrapper.Syntax
     public interface ISyntax<TExtension> : IEnumerable<IExecutable<TExtension>>
         where TExtension : IExtension
     {
+    }
+
+    /// <summary>
+    /// Execute action syntax.
+    /// </summary>
+    /// <typeparam name="TExtension">The type of the extension.</typeparam>
+    public interface IExecuteAction<TExtension> : ISyntax<TExtension>
+        where TExtension : IExtension
+    {
+        /// <summary>
+        /// Adds an execution action to the currently built syntax.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <returns>The current syntax builder.</returns>
+        IWithBehavior<TExtension> Execute(Action action);
+    }
+
+    /// <summary>
+    /// Execute an action on an extension syntax.
+    /// </summary>
+    /// <typeparam name="TExtension">The type of the extension.</typeparam>
+    public interface IExecuteActionOnExtension<TExtension> : ISyntax<TExtension>
+        where TExtension : IExtension
+    {
+        /// <summary>
+        /// Adds an execution action which operates on the extension to the
+        /// currently built syntax.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <returns>The current syntax builder.</returns>
+        IWithBehavior<TExtension> Execute(Action<TExtension> action);
+    }
+
+    /// <summary>
+    /// Execute an action on an extension with a context syntax.
+    /// </summary>
+    /// <typeparam name="TExtension">The type of the extension.</typeparam>
+    public interface IExecuteActionOnExtensionWithContext<TExtension> : ISyntax<TExtension>
+        where TExtension : IExtension
+    {
+        /// <summary>
+        /// Adds an context initializer and an execution action which gets
+        /// access to the context to the currently built syntax.
+        /// </summary>
+        /// <typeparam name="TContext">The type of the context.</typeparam>
+        /// <param name="initializer">The context initializer.</param>
+        /// <param name="action">The action with access to the context.</param>
+        /// <returns>
+        /// The current syntax builder.
+        /// </returns>
+        IWithBehaviorOnContext<TExtension, TContext> Execute<TContext>(Func<TContext> initializer, Action<TExtension, TContext> action);
+    }
+
+    /// <summary>
+    /// Fluent definition syntax interface for behaviors.
+    /// </summary>
+    /// <typeparam name="TExtension">The type of the extension.</typeparam>
+    public interface IWithBehavior<TExtension> : IExecuteAction<TExtension>, IExecuteActionOnExtension<TExtension>, IExecuteActionOnExtensionWithContext<TExtension>
+        where TExtension : IExtension
+    {
+        /// <summary>
+        /// Attaches a behavior to the currently built executable.
+        /// </summary>
+        /// <param name="behavior">The behavior.</param>
+        /// <returns>
+        /// The syntax.
+        /// </returns>
+        IWithBehavior<TExtension> With(IBehavior<TExtension> behavior);
+
+        /// <summary>
+        /// Attaches a lazy behavior to the currently built executable.
+        /// </summary>
+        /// <param name="behavior">The behavior.</param>
+        /// <returns>
+        /// The syntax.
+        /// </returns>
+        IWithBehavior<TExtension> With(Func<IBehavior<TExtension>> behavior);
+    }
+
+    /// <summary>
+    /// Fluent definition syntax interface for behaviors which operate on contexts.
+    /// </summary>
+    /// <typeparam name="TExtension">The type of the extension.</typeparam>
+    /// <typeparam name="TContext">The type of the context.</typeparam>
+    public interface IWithBehaviorOnContext<TExtension, out TContext> : IExecuteAction<TExtension>, IExecuteActionOnExtension<TExtension>, IExecuteActionOnExtensionWithContext<TExtension>
+        where TExtension : IExtension
+    {
+        /// <summary>
+        /// Attaches a behavior which has access to the context to the currently built executable.
+        /// </summary>
+        /// <param name="provider">The behavior provider.</param>
+        /// <returns>The syntax.</returns>
+        IWithBehaviorOnContext<TExtension, TContext> With(Func<TContext, IBehavior<TExtension>> provider);
     }
 }
